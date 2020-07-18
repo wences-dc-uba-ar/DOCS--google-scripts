f4d56f2b707e45a8ecb118c3bf4febd1d576d32d
Wenceslao A. Zuloaga <waz100k@gmail.com>
2020-07-18 17:43:08 -0300

    + timestamp if there is a column 'updated'

--- spreadsheets/autosort.js
+++ spreadsheets/autosort.js
@@ -1,7 +1,6 @@
 /*
 
 TODO:
-- add timestamp if there is a column 'updated'
 - auto expand sheet to fit range
 - auto shrink sheet to range if the rest is empty
 - add documentation
@@ -24,6 +23,7 @@ function onEdit(event) {
     if (fName == 'autosort') {
         colorizeRange2(sheet, sRange)
         let range = sheet.getRange(sRange)
+        update_timestamp(sheet, range)
         let orders = (sOrder || '+').split("")
         let sorters = orders.reduce(function (newSorter, symbol, index) {
             if ('-+'.includes(symbol)) {
@@ -59,4 +59,21 @@ function colorizeRange(range) {
 
 function r16(n) {
     return (Math.floor(n) % 16 + 16) % 16
+}
+
+
+function update_timestamp(sheet, sortRange) {
+    let titles = sheet.getRange(sortRange.getRow() - 1, sortRange.getColumn(), 1, sortRange.getWidth()).getValues()
+    for (var i in titles[0]) {
+        if(titles[0][i].toLowerCase() == 'updated') {
+            set_mtime(sheet, parseInt(i)+1)
+        }
+    }
+}
+
+function set_mtime(sheet, y) {
+    let x = sheet.getCurrentCell().getRow()
+    let tzoffset = (new Date()).getTimezoneOffset() * 60000;
+    let timeString = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -8).replace('T', ' ')
+    sheet.getRange(x,y).setValue(timeString)
 }
